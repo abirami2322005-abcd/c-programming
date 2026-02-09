38a447627c6da34b90ea446e81fcb70418876738
abirami2322005-abcd <abirami2322005@gmail.com>
2026-02-09 19:27:57 +0530

    Add files via upload

---
+++ If else.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int num;
+    scanf("%d",&num);
+    if (num%2==0) {
+        printf("Even");
+    }
+    if (num%2!=0) {
+        printf("Odd");
+    }
+    return 0;
+}
