89b1fe8e5f6d27aa2189f48dc8d8a2775c308d91
abirami2322005-abcd <abirami2322005@gmail.com>
2026-02-16 15:37:14 +0530

    Add files via upload

--- nestedloop7.c
+++ nestedloop7.c
@@ -1,11 +1,9 @@
 #include<stdio.h>
 int main(){
-    for (int i=1;i<=4;i++){
-        for (int k=1;k<=4-i;k++){
-            printf(" ");
-        }
-        for (int j=1;j<=2*i-1;j++){
-            printf("*");
+    char ch='A';
+    for(int i=0;i<=5;i++){
+        for(int j=0;j<=i;j++){
+            printf("%c",ch+i);
         }
         printf("\n");
     }
