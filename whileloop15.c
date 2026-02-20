e426b378e5a2936da747c8ce6e19933a7362fa7b
abirami2322005-abcd <abirami2322005@gmail.com>
2026-02-20 19:04:00 +0530

    Add files via upload

---
+++ whileloop15.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main() {
+    int N;
+    scanf("%d", &N);
+    int hours;
+    int i = 0;
+    int totalOvertime = 0;
+    int burnoutDays = 0;
+    i = 0;
+    while(i < N) {
+        scanf("%d",&hours);
+        totalOvertime += hours;
+        if(hours > 4) {
+            burnoutDays++;
+        }
+        i++;
+    }
+    printf("Total Overtime: %d\n", totalOvertime);
+    printf("Burnout Days: %d\n", burnoutDays);
+    return 0;
+}
