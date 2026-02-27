15db9d50a2a6da942deacce0f7141b1271625600
abirami2322005-abcd <abirami2322005@gmail.com>
2026-02-27 13:57:11 +0530

    Add files via upload

---
+++ array2d3.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d%d",&r,&c);
+
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            for(int k=i; k<r; k++){
+                for(int l=0; l<c; l++){
+                    if(k==i && l<=j) 
+                        continue;
+                    if(arr[i][j] == arr[k][l]){
+                        printf("%d", arr[i][j]);
+                        return 0;
+                    }
+                }
+            }
+        }
+    }
+}
