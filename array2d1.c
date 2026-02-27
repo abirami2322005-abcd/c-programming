15840da37396b921bc0259c8dbed6ca4ecc229ff
abirami2322005-abcd <abirami2322005@gmail.com>
2026-02-27 13:25:48 +0530

    Add files via upload

---
+++ array2d1.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int r,c;
+    scanf("%d%d",&r,&c);
+    int arr[r][c];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            scanf("%d",&arr[i][j]);
+        }
+    }
+    int maxproduct=arr[0][0]*arr[1][0];
+    for(int i=0;i<r;i++){
+        for(int j=0;j<c;j++){
+            for(int k=i+1;k<r;k++){
+                for(int l=0;l<c;l++){
+                    int product=arr[i][j]*arr[k][l];
+                    if(product>maxproduct){
+                        maxproduct=product;
+                    }
+                }
+            }
+        }
+    }
+    printf("%d is maxvalue product",maxproduct);
+    return 0;
+}
